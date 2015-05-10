3a8020ab24c709705a342105f556841219426edb
Oginga <stevilley@gmail.com>
2015-05-10 23:23:24 +0300

    odel created

---
+++ src/modules/models/ParentModel.php
@@ -0,0 +1,37 @@
+<?php 
+namespace AngryCoders\HelpInfo\modules\models;
+use AngryCoders\Db\Db;
+use AngryCoders\Db\DbException;
+
+class ParentModel{
+	
+	
+	
+	/*
+	 * Create database helpInfo table
+	 * */
+	public function createTable(){
+		
+		$db=new Db();
+	
+		try {
+		
+			$db->createTable("HelpInfo", array(
+					"helpInfoID" => array(Db::INTEGER, 11, Db::PRIMARY_KEY, Db::AUTO_INCREMENT),
+					"title" => array(Db::VARCHAR, 50),
+					"description" => array(Db::TEXT,200)
+			)
+			);
+		
+		} catch (DbException $e) {
+			echo $e;
+		}
+	}
+	
+	
+	
+	
+	
+}
+
+?>
